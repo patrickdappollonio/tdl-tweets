c4648a2bfb526395db1bab045f275498ff0ffc36
Patrick D'appollonio <patrick@dappollonio.us>
2016-07-09 14:30:55 -0600

    Adds support for twitch Client ID.

--- requests.go
+++ requests.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/urlfetch"
 )
 
-func Request(ctx context.Context, url string) (*http.Response, error) {
-	client := urlfetch.Client(ctx)
-	return client.Get(url)
+const TWITCH_API_HOST = "api.twitch.tv"
+
+func Request(ctx context.Context, weburl string) (*http.Response, error) {
+	u, err := url.Parse(url)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse url: %v", weburl)
+	}
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct an HTTP request: %v", err.Error())
+	}
+
+	if u.Host == TWITCH_API_HOST {
+		req.Header.Add("Client-ID", TWITCH_CLIENT_ID)
+	}
+
+	return urlfetch.Client(ctx).Do(req)
 }
